cf656e82c8b7202d6e0a9b4dab6ea470f0c22cb6
Ladas125 <ladas215@gmail.com>
2016-06-29 15:49:55 +0200

    NetworkJob.java moved into custom package and ContextDataWork.java, ContextUpdateWork.java moved from static inner classes to custom files;

---
+++ src/main/java/eu/codetopic/utils/thread/job/network/ContextDataWork.java
@@ -0,0 +1,71 @@
+package eu.codetopic.utils.thread.job.network;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.UiThread;
+import android.support.annotation.WorkerThread;
+
+import java.lang.ref.WeakReference;
+
+import eu.codetopic.utils.Log;
+import eu.codetopic.utils.thread.JobUtils;
+
+public abstract class ContextDataWork<C extends Context, D> implements NetworkJob.Work {
+
+    private static final String LOG_TAG = NetworkJob.LOG_TAG + "$ContextDataWork";
+
+    @NonNull
+    private final Context appContext;
+    @NonNull
+    private final WeakReference<C> contextRef;
+
+    public ContextDataWork(@NonNull C context) {
+        this.appContext = context.getApplicationContext();
+        this.contextRef = new WeakReference<>(context);
+    }
+
+    @NonNull
+    public Context getAppContext() {
+        return appContext;
+    }
+
+    @Nullable
+    @UiThread
+    public C getContext() {
+        return contextRef.get();
+    }
+
+    @NonNull
+    public WeakReference<C> getContextRef() {
+        return contextRef;
+    }
+
+    @Override
+    @WorkerThread
+    public final void run() throws Throwable {
+        D result = null;
+        Throwable throwable = null;
+        try {
+            result = work();
+        } catch (final Throwable t) {
+            Log.d(LOG_TAG, "run", t);
+            throwable = t;
+        } finally {
+            final D fResult = result;
+            final Throwable fThrowable = throwable;
+            JobUtils.runOnContextThread(getContextRef().get(), new Runnable() {
+                @Override
+                public void run() {
+                    result(fResult, fThrowable);
+                }
+            });
+        }
+    }
+
+    @WorkerThread
+    public abstract D work() throws Throwable;
+
+    @UiThread
+    public abstract void result(@Nullable D data, @Nullable Throwable tr);
+}
---
+++ src/main/java/eu/codetopic/utils/thread/job/network/ContextUpdateWork.java
@@ -0,0 +1,53 @@
+package eu.codetopic.utils.thread.job.network;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.UiThread;
+
+public abstract class ContextUpdateWork<C extends Context, D> extends ContextDataWork<C, D> {
+
+    public ContextUpdateWork(@NonNull C context) {
+        super(context);
+    }
+
+    @Override
+    public final void result(@Nullable D data, @Nullable Throwable tr) {
+        if (tr != null) error(tr);
+        else update(data);
+    }
+
+    @UiThread
+    public void update(D data) {
+        C context = getContext();
+        if (context != null) updateContext(context, data);
+        else updateNoContext(data);
+    }
+
+    @UiThread
+    public void updateContext(@NonNull C context, D data) {
+
+    }
+
+    @UiThread
+    public void updateNoContext(D data) {
+
+    }
+
+    @UiThread
+    public void error(Throwable tr) {
+        C context = getContext();
+        if (context != null) errorContext(context, tr);
+        else errorNoContext(tr);
+    }
+
+    @UiThread
+    public void errorContext(@NonNull C context, Throwable tr) {
+
+    }
+
+    @UiThread
+    public void errorNoContext(Throwable tr) {
+
+    }
+}
