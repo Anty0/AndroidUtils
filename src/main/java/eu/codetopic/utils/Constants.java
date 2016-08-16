4ad4fb3303744d56c08701d0975bd3bf3ab8f0bf
Anty0 <anty150@gmail.com>
2016-08-16 17:26:47 +0200

    fixed Constants constructor and added final modifier;

--- src/main/java/eu/codetopic/utils/Constants.java
+++ src/main/java/eu/codetopic/utils/Constants.java
@@ -1,12 +1,13 @@
 package eu.codetopic.utils;
 
-public class Constants {
+public final class Constants {
 
     public static final int JOB_REPEAT_COUNT_DEFAULT = 10;
     public static final int JOB_REPEAT_COUNT_NETWORK = 5;
     public static final int JOB_REPEAT_COUNT_DATABASE = 1;
     public static final int JOB_PRIORITY_NETWORK = 5;
     public static final int JOB_PRIORITY_DATABASE = 10;
-    //
-    public static final int WIDGET_HOST_ID = 148297;
+    private Constants() {
+    }
+
 }
