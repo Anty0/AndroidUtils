6f8e4eecdbbd8809c067f0d35c33a919dabe36d2
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2017-11-15 09:18:02 +0100

    create GsonPreference variable delegate;

---
+++ src/main/java/eu/codetopic/utils/data/preferences/support/GsonPreference.kt
@@ -0,0 +1,61 @@
+/*
+ * ApplicationPurkynka
+ * Copyright (C)  2017  anty
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package eu.codetopic.utils.data.preferences.support
+
+import com.google.gson.Gson
+import eu.codetopic.java.utils.log.Log
+import java.lang.reflect.Type
+import kotlin.reflect.KProperty
+
+/**
+ * @author anty
+ */
+class GsonPreference<T>(val key: String, private val gson: Gson, private val typeOfT: Type,
+                        private val preferencesAccessor: IPreferencesAccessor<*>,
+                        private val defaultValue: () -> T) {
+
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        synchronized(thisRef ?: this) {
+            try {
+                return gson.fromJson(
+                        preferencesAccessor.preferences.getString(key, null)
+                                ?: return defaultValue(),
+                        typeOfT)
+            } catch (e: Exception) {
+                Log.e(preferencesAccessor.name, "$key: Failed to load", e)
+                return defaultValue()
+            }
+        }
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        synchronized(thisRef ?: this) {
+            try {
+                preferencesAccessor.edit {
+                    putString(key, gson.toJson(value))
+                }
+            } catch (e: Exception) {
+                Log.e(preferencesAccessor.name, "$key: Failed to save", e)
+            }
+        }
+    }
+
+    val isSet: Boolean @Synchronized get() = preferencesAccessor.preferences.contains(key)
+}
