fb7da3699781bcbff4c22d2bb4d9e98e28a089f6
anty <anty150@gmail.com>
2018-01-04 00:35:01 +0100

    add SimpleTransitionListener;

---
+++ src/main/java/eu/codetopic/utils/simple/SimpleTransitionListener.kt
@@ -0,0 +1,33 @@
+/*
+ * utils
+ * Copyright (C)   2018  anty
+ *
+ * This program is free  software: you can redistribute it and/or modify
+ * it under the terms  of the GNU General Public License as published by
+ * the Free Software  Foundation, either version 3 of the License, or
+ * (at your option) any  later version.
+ *
+ * This program is distributed in the hope that it  will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied  warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+ * GNU General Public License for more details.
+ *
+ * You  should have received a copy of the GNU General Public License
+ * along  with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package eu.codetopic.utils.simple
+
+import android.transition.Transition
+
+/**
+ * @author anty
+ */
+open class SimpleTransitionListener : Transition.TransitionListener {
+
+    override fun onTransitionEnd(transition: Transition) {}
+    override fun onTransitionResume(transition: Transition) {}
+    override fun onTransitionPause(transition: Transition) {}
+    override fun onTransitionCancel(transition: Transition) {}
+    override fun onTransitionStart(transition: Transition) {}
+}
